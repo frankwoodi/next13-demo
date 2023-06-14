306efe378d33e0f1e821dad500e03133f2829dda
amar-jay <64834413+amar-jay@users.noreply.github.com>
2023-06-14 18:38:13 +0300

    default export error

--- my-app/src/app/todolist/page.tsx
+++ my-app/src/app/todolist/page.tsx
@@ -1,28 +1,24 @@
-import List from "./(list)/List"
-import { Input } from "./(input)/Input"
-interface ListProps {
-    name: string,
-    id: string,
-    done: boolean
+import List from "./(list)/List";
+import { Input } from "./(input)/Input";
+export interface ListProps {
+  name: string;
+  id: string;
+  done: boolean;
 }
-function Page({name, id}: ListProps) {
+export default function Page({ name, id }: ListProps) {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>
+        <h1 className="text-5xl text-center text-primary underline">
+          Todo List
+        </h1>
+        <p className="text-center"> a list of todos </p>
+      </div>
+      <Input />
+      <List />
 
-
-    return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <div>
-            <h1 className="text-5xl text-center text-primary underline">Todo List</h1>
-            <p className="text-center"> a list of todos </p>
-            
-        </div>
-        <Input/>
-        <List/>
-
-        
-        <div/>
-        <div/>
-        </main>
-        )
+      <div />
+      <div />
+    </main>
+  );
 }
-
-export default Page
